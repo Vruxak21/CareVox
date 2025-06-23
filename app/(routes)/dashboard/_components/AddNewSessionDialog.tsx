5b931e281d03511644035f4831218140d7be27a3
Vruxak21 <patelvruxak21@gmail.com>
2025-06-24 00:43:37 +0530

    Suggesting doctor and storing to databse successfull

--- app/(routes)/dashboard/_components/AddNewSessionDialog.tsx
+++ app/(routes)/dashboard/_components/AddNewSessionDialog.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { useState } from "react";
 import {
   Dialog,
@@ -13,10 +13,37 @@ import {
 import { Button } from "@/components/ui/button";
 import { IconPlus } from "@tabler/icons-react";
 import { Textarea } from "@/components/ui/textarea";
-import { ArrowRight } from "lucide-react";
+import { ArrowRight, Loader2 } from "lucide-react";
+import axios from "axios";
+import DoctorAgentCard, { doctorAgent } from "./DoctorAgentCard";
+import SuggestedDoctorCard from "./SuggestedDoctorCard";
 
 function AddNewSessionDialog() {
-    const [note, setNote] = useState<string>();
+  const [note, setNote] = useState<string>();
+  const [loading, setLoading] = useState(false);
+  const [suggestedDoctors, setSuggestedDoctors] = useState<doctorAgent[]>();
+  const [selectedDoctor, setSelectedDoctor] = useState<doctorAgent>();
+  const OnClickNext = async () => {
+    setLoading(true);
+    const result = await axios.post("/api/suggest-doctors", {
+      notes: note,
+    });
+    console.log(result.data);
+    setSuggestedDoctors(result.data);
+    setLoading(false);
+  };
+  const onStartConsultation = async() => {
+    setLoading(true);
+    const result = await axios.post('/api/session-chat', {
+      notes: note,
+      selectedDoctor: selectedDoctor
+    })
+    console.log(result.data);
+    if(result.data?.sessionId) {
+      console.log(result.data.sessionId);
+    }
+    setLoading(false);
+  }
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -29,17 +56,45 @@ function AddNewSessionDialog() {
         <DialogHeader>
           <DialogTitle>Add Basic Details</DialogTitle>
           <DialogDescription asChild>
-            <div>
-              <h2>Add Symptoms or Any Other Details</h2>
-              <Textarea placeholder="Add details here..." className="h-[200px] mt-1" onChange={(e)=>setNote(e.target.value)}/>
-            </div>
+            {!suggestedDoctors ? (
+              <div>
+                <h2>Add Symptoms or Any Other Details</h2>
+                <Textarea
+                  placeholder="Add details here..."
+                  className="h-[200px] mt-1"
+                  onChange={(e) => setNote(e.target.value)}
+                />
+              </div>
+            ) : (
+              <div>
+                <h2>Select the Doctor</h2>
+                <div className="grid grid-cols-3 gap-5 mt-1">
+                  {suggestedDoctors.map((doctor, index) => (
+                    <SuggestedDoctorCard
+                      doctorAgent={doctor}
+                      key={index}
+                      setSelectedDoctor={() => setSelectedDoctor(doctor)}
+                      //@ts-ignore
+                      selectedDoctor={selectedDoctor}
+                    />
+                  ))}
+                </div>
+              </div>
+            )}
           </DialogDescription>
         </DialogHeader>
         <DialogFooter>
-            <DialogClose asChild>
-                <Button variant={'outline'}>Cancel</Button>
-            </DialogClose>
-            <Button disabled={!note}>Next <ArrowRight/></Button>
+          <DialogClose asChild>
+            <Button variant={"outline"}>Cancel</Button>
+          </DialogClose>
+          {!suggestedDoctors ? (
+            <Button disabled={!note || loading} onClick={() => OnClickNext()}>
+              Next{" "}
+              {loading ? <Loader2 className="animate-spin" /> : <ArrowRight />}
+            </Button>
+          ) : (
+            <Button disabled={loading || !selectedDoctor} onClick={()=>onStartConsultation()}>Start Consultation {loading ? <Loader2 className="animate-spin" /> : <ArrowRight />}</Button>
+          )}
         </DialogFooter>
       </DialogContent>
     </Dialog>
---
+++ app/api/suggest-doctors/route.tsx
@@ -0,0 +1,32 @@
+import { openai } from "@/config/OpenAIModel";
+import { AIDoctorAgents } from "@/shared/list";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const { notes } = await req.json();
+  try {
+    const completion = await openai.chat.completions.create({
+      model: "google/gemini-2.0-flash-exp:free",
+      messages: [
+        { role: "system", content: JSON.stringify(AIDoctorAgents) },
+        {
+          role: "user",
+          content:
+            "User Notes/Symptoms: " +
+            notes +
+            ", Depends on user notes and symptoms, please suggest list of doctors, Return object in json only",
+        },
+      ],
+    });
+    const rawResp = completion.choices[0].message;
+    //@ts-ignore
+    const Resp = rawResp.content
+      .trim()
+      .replace("```json", "")
+      .replace("```", "");
+    const JSONResp = JSON.parse(Resp);
+    return NextResponse.json(JSONResp);
+  } catch (e) {
+    return NextResponse.json(e);
+  }
+}
